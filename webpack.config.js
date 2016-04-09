aaa7e187a18577cf259124c726ebe0775536d587
Michael Burson <michael.burson@razoroccam.com>
2016-04-09 18:11:35 +0100

    Webpack setup with react

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './src/index'
+  ],
+  output: {
+    publicPath: '/',
+    filename: 'main.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react', 'stage-0']
+        }
+      }
+    ]
+  },
+  debug: true,
+  devServer: {
+    contentBase: "./src"
+  }
+}
